88d9ef2328ac4b8913f964b80da3c12ed61cbdb9
Mario Barboza <90014477+marioBarbozaA@users.noreply.github.com>
2023-09-28 19:31:10 -0600

    hice el login

--- duende-pagina-web/src/App.js
+++ duende-pagina-web/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import './Assets/tipografias.css';
+import Login from'./Components/Login/Login.js';
+import Register from './Components/Register/Register.js';
+//<Login/>
 
 function App() {
   return (<>
-     
-  
+     <div className="App">
+      <Register/>
+     </div>
   </>
  
   );
