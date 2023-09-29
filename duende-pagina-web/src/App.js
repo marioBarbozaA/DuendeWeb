23cb246ee0aa7713d2e4178319298fdb158e9e4a
Mario Barboza <90014477+marioBarbozaA@users.noreply.github.com>
2023-09-28 19:58:07 -0600

    Recuperacion listo

--- duende-pagina-web/src/App.js
+++ duende-pagina-web/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import './Assets/tipografias.css';
 import Login from'./Components/Login/Login.js';
 import Register from './Components/Register/Register.js';
+import Recuperacion from './Components/Recuperacion/Recuperacion.js';
 //<Login/>
 
 function App() {
   return (<>
      <div className="App">
-      <Login/>
+      <Recuperacion/>
      </div>
   </>
  
