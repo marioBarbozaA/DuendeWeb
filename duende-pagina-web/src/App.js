761bda375e68bbc9bc9d04b7720d4f6f2ffe9171
Mario Barboza <90014477+marioBarbozaA@users.noreply.github.com>
2023-09-28 20:25:59 -0600

    Update App.js

--- duende-pagina-web/src/App.js
+++ duende-pagina-web/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './Assets/tipografias.css';
-import Login from'./Components/GestionUsuarios/Login/Login.js';
-import Register from './Components/GestionUsuarios/Register/Register.js';
+//import Login from'./Components/GestionUsuarios/Login/Login.js';
+//import Register from './Components/GestionUsuarios/Register/Register.js';
 import Recuperacion from './Components/GestionUsuarios/Recuperacion/Recuperacion.js';
 //<Login/>
 
