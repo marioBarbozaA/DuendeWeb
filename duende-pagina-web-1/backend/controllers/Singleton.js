2bb2d41c1bd50f63290795df87cc354ae95a6b38
Nahomi Bolaños <120617689+nahomibv@users.noreply.github.com>
2023-10-15 22:46:20 -0600

    Add GalleryImage Functions to Singleton.js

--- duende-pagina-web-1/backend/controllers/Singleton.js
+++ duende-pagina-web-1/backend/controllers/Singleton.js
@@ -28,6 +28,114 @@ class Singleton {
         this.count = this.count + 1;
         return this.instance;
     }
+    //-------------------------------------------------------------------------------------
+    //                               GalleryImage Functions
+    //-------------------------------------------------------------------------------------
+    async addGalleryImage(req, res, next) {
+        try {
+            const jsonImage = req.body;
+            await GalleryImage.create({
+                "name": jsonImage.name,
+                "category": jsonImage.category,
+                "subCategory": jsonImage.subCategory,
+                "description": jsonImage.description,
+                "date": jsonImage.date,
+                "tags": jsonImage.tags,
+                "mainImage": jsonImage.mainImage,
+                "images": jsonImage.images,
+                "status": jsonImage.status
+            });
+    
+            res.status(201).json({ state: true, message: 'Se ha agregado la imagen exitosamente' });
+        } catch (error) {
+            res.status(500).json({ message: `Error del servidor: ${error}` });
+        }
+        next();
+    }
+
+    async updateGalleryImage(req, res, next) {
+        try {
+            const jsonImage = req.body; 
+
+            await GalleryImage.updateOne({ _id: jsonImage._id }, {
+                $set: {
+                    "name": jsonImage.name,
+                    "category": jsonImage.category,
+                    "subCategory": jsonImage.subCategory,
+                    "description": jsonImage.description,
+                    "date": jsonImage.date,
+                    "tags": jsonImage.tags,
+                    "mainImage": jsonImage.mainImage,
+                    "images": jsonImage.images,
+                    "status": jsonImage.status
+                }
+            });
+    
+            res.status(200).json({ state: true, message: 'La imagen se ha modificado exitosamente' });
+        } catch (error) {
+            res.status(500).json({ message: `Error del servidor: ${error}` });
+        }
+        next();
+    }
+
+    async deleteGalleryImage(req, res, next) {
+        try {
+            const jsonImage = req.body; 
+    
+            const imageFound = await GalleryImage.findOne({ _id: jsonImage.imageId });
+    
+            if (!imageFound) {
+                return res.status(404).json({ message: 'La imagen no se encuentra' });
+            }
+    
+            await GalleryImage.deleteOne({ _id: jsonImage.imageId });
+    
+            res.status(200).json({ state: true, message: 'La imagen se ha eliminado exitosamente' });
+        } catch (error) {
+            res.status(500).json({ message: `Error del servidor: ${error}` });
+        }
+        next();
+    }
+
+    async getGalleryImagesByCategory(req, res, next) {
+        try {
+            const category = req.params.category; 
+            const images = await GalleryImage.find({ category });
+    
+            if (images.length === 0) {
+                return res.status(404).json({ message: 'No se encontraron imágenes en esta categoría' });
+            }
+    
+            res.status(200).json(images);
+        } catch (error) {
+            res.status(500).json({ message: `Error del servidor: ${error}` });
+        }
+        next();
+    }
+
+    async getAllImages(req, res, next) {
+        try {
+            const images = await GalleryImage.find({});
+    
+            if (images.length === 0) {
+                return res.status(404).json({ message: 'No se encontraron imágenes en la galería' });
+            }
+    
+            res.status(200).json(images);
+        } catch (error) {
+            res.status(500).json({ message: `Error del servidor: ${error}` });
+        }
+        next();
+    }
+
+
+    //-------------------------------------------------------------------------------------
+    //                                 Message Functions
+    //-------------------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------------------
+    //                                Appointment Functions
+    //-------------------------------------------------------------------------------------
 
     async registerUser(req, res, next) {
         next();
@@ -36,3 +144,6 @@ class Singleton {
 
 const dao = Singleton.getInstance();
 module.exports = dao;
+
+
+
