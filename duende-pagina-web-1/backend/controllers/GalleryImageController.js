37a16d128ee516180d11e9a8f769822b4b619a14
Nahomi Bolaños <120617689+nahomibv@users.noreply.github.com>
2023-10-17 18:50:24 -0600

    Add functions to GalleryImageController.js
    Added Singleton function calls without validating the body yet.

--- duende-pagina-web-1/backend/controllers/GalleryImageController.js
+++ duende-pagina-web-1/backend/controllers/GalleryImageController.js
@@ -1,1 +1,36 @@
 const SingletonDAO = require('./Singleton.js');
+
+
+const addGalleryImage = async (req, res, next) => {
+    await Singleton.addGalleryImage(req, res, next);
+  };
+  
+  const updateGalleryImage = async (req, res, next) => {
+    await Singleton.updateGalleryImage(req, res, next);
+  };
+  
+  const deleteGalleryImage = async (req, res, next) => {
+    await Singleton.deleteGalleryImage(req, res, next);
+  };
+  
+  const getGalleryImageByID = async (req, res, next) => {
+    await Singleton.getGalleryImageByID(req, res, next);
+  };
+  
+  const getGalleryImagesByCategory = async (req, res, next) => {
+    await Singleton.getGalleryImagesByCategory(req, res, next);
+  };
+  
+  const getAllImages = async (req, res, next) => {
+    await Singleton.getAllImages(req, res, next);
+  };
+  
+  module.exports = {
+    addGalleryImage,
+    updateGalleryImage,
+    deleteGalleryImage,
+    getGalleryImageByID,
+    getGalleryImagesByCategory,
+    getAllImages,
+  };
+  
