b46e08c9de6a4b1a24636c4129880b0443fca5ec
Nahomi Bolaños <120617689+nahomibv@users.noreply.github.com>
2023-10-17 18:57:28 -0600

    Add functions to AppointmentController.js
    Added Singleton function calls without validating the body yet.

--- duende-pagina-web-1/backend/controllers/AppointmentController.js
+++ duende-pagina-web-1/backend/controllers/AppointmentController.js
@@ -1,2 +1,29 @@
 const Singelton = require('./Singleton.js');
 
+const createAppointment = async (req, res, next) => {
+    await Singleton.createAppointment(req, res, next);
+  };
+  
+  const updateAppointment = async (req, res, next) => {
+    await Singleton.updateAppointment(req, res, next);
+  };
+  
+  const deleteAppointment = async (req, res, next) => {
+    await Singleton.deleteAppointment(req, res, next);
+  };
+  
+  const getAppointmentById = async (req, res, next) => {
+    await Singleton.getAppointmentById(req, res, next);
+  };
+  
+  const getAllAppointments = async (req, res, next) => {
+    await Singleton.getAllAppointments(req, res, next);
+  };
+  
+  module.exports = {
+    createAppointment,
+    updateAppointment,
+    deleteAppointment,
+    getAppointmentById,
+    getAllAppointments,
+  };
