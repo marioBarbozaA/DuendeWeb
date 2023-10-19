fd95416e8878780183bc9eca1a58107f93dd490c
Nahomi Bolaños <120617689+nahomibv@users.noreply.github.com>
2023-10-19 17:00:55 -0600

    Add GalleryImage routes to GalleryImageRoute.js

--- duende-pagina-web-1/backend/routes/GalleryImageRoute.js
+++ duende-pagina-web-1/backend/routes/GalleryImageRoute.js
@@ -3,4 +3,24 @@ const router = express.Router();
 const Gallery = require('./../controllers/GalleryImageController.js');
 
 
+
+
+router.route('/create')
+    post(Gallery.addGalleryImage); 
+
+router.route('/update')
+    post(Gallery.updateGalleryImage);
+
+router.route('/delete')
+    .post(deleteGalleryImage);
+
+router.route('/getGalleryImageByID')
+    .get(getGalleryImageByID);
+
+router.route('/getGalleryImagesByCategory')
+    .get(getGalleryImagesByCategory);
+
+router.route('/getAllImages')
+    .get(getAllImages);
+
 module.exports = router;
