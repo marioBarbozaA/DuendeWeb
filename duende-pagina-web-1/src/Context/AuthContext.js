99d2b1816553137783ff31eaa990561750d7d876
Mario Barboza <90014477+marioBarbozaA@users.noreply.github.com>
2023-10-24 22:43:37 -0600

    Creacion de Contexto

--- duende-pagina-web-1/src/Context/AuthContext.js
+++ duende-pagina-web-1/src/Context/AuthContext.js
@@ -1,18 +1,33 @@
-import { createContext, useState } from 'react';
-import { registerRequest } from '../api/auth';
+import { createContext, useState, useContext } from 'react';
+import { handleRegister } from '../pages/GestionUsuarios/Register/Register.js';
 
 export const AuthContext = createContext();
 
+export const useAuth = () => {
+	const context = useContext(AuthContext);
+	if (!context) {
+		throw new Error('useAuth is out from provider');
+	}
+	return context;
+};
+
+//Este va a ser el usuario General que ingresa
 export const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
-
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
 	const signup = async user => {
-		const res = await registerRequest(user);
-		console.log(res.data);
-		setUser(res.data);
+		try {
+			console.log('se ace en signup');
+			const res = await handleRegister(user);
+			console.log(res.data);
+			setUser(res.data);
+			setIsAuthenticated(true);
+		} catch (error) {
+			console.log(error);
+		}
 	};
 	return (
-		<AuthContext.Provider value={{ signup, user }}>
+		<AuthContext.Provider value={{ signup, user, isAuthenticated }}>
 			{children}
 		</AuthContext.Provider>
 	);
